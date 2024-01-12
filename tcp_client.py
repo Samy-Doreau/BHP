b653d518af9fc77336a07b0024f2b6c895866b92
Samy <samydoreau@gmail.com>
2024-01-12 19:33:20 +0000

    Initial files

---
+++ tcp_client.py
@@ -0,0 +1,17 @@
+import socket
+
+
+target_host = '0.0.0.0'
+target_port = 9998
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((target_host, target_port))
+
+client.send(b"GET / HTTP/1.1\r\n Host: google.com\r\n\r\n")
+
+response = client.recv(4096)
+
+print(response.decode())
+
+client.close()
